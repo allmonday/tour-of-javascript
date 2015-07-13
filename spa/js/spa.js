c2a1f18061900cedaa20991cf50060879d086835
tangkikodo <allmonday@126.com>
2015-07-13 22:04:12 +0800

    more

---
+++ spa/js/spa.js
@@ -0,0 +1,11 @@
+var spa = (function ($) {
+	initModule = function ($container) {
+		$container.html(
+			'<h1 style="display:inline-block; margin:25px;">'
+			+ 'hello world'
+			+ '</h1>'
+		);
+	};
+
+	return { initModule: initModule };
+}());
